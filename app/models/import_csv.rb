df1ddb8ae45895fa2a49559282141bd3e66025da
marim0ss <piyomaru.bunbun@gmail.com>
2018-11-26 17:10:57 +0900

    配列listを使って一括読み込み→保存に変更

--- app/models/import_csv.rb
+++ app/models/import_csv.rb
@@ -3,12 +3,18 @@
 class ImportCsv < ApplicationRecord
   # CSVデータのパスを引数として受け取り、インポート処理を実行
   def self.import(path)
+
+    # インポートするデータを格納するための配列
+    list = []
+
      CSV.foreach(path, headers: true) do |row|
-       User.create(
+       list << {
          name: row["name"],
          age: row["age"],
          address: row["address"]
-       )
+       }
      end
+
+     User.create(list)
   end
 end
